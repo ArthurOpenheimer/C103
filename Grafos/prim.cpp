cea75b0039fd551e10a1779e3814bf2c8a6bbdf8
Arthur Openheimer <arthur.openheimer@pdsolucoes.com.br>
2022-11-01 10:48:20 -0300

    feat: adicionar algoritmo de Prim

---
+++ Grafos/prim.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<list>
+#define INT_MAX 999
+
+using namespace std;
+
+struct no{
+    int v; //destino
+    int p; //peso
+};
+
+void cria_aresta(list<no>adj[], int u, int v, int p, int orientado){
+    no aux; //auxiliar para inserir arestas na lista
+
+    aux.v = v;
+    aux.p = p;
+
+    adj[u].push_back(aux);
+    //inserindo aresta da volta
+    if(orientado == 0){
+        aux.v = u;
+        adj[v].push_back(aux);
+    }
+}
+
+void prim(list<no>adj[], int nVertices, int start){
+
+    int distancia[nVertices];
+    int pai[nVertices];
+    int intree[nVertices];
+    int v, destino, weight, dist;
+    int custo = 0;
+    int i;
+    list<no>:: iterator q; 
+
+    for(i=0; i<nVertices; i++){
+        distancia[i] = INT_MAX;
+        pai[i] = -1;
+        intree[i] = 0;
+    }
+
+    distancia[start] = 0;
+    v = start;
+
+    while(intree[v] == false){
+        intree [v] = true;
+        for(q=adj[v].begin(); q!=adj[v].end(); q++){
+            destino = q->v;
+            weight = q->p;
+            if((distancia[destino] > weight) && (intree[destino] == false)){
+                distancia[destino] = weight;
+                pai[destino] = v;
+            }
+        }
+
+        v = 0;
+        dist = INT_MAX;
+        for(i=0; i<nVertices; i++){
+            if((distancia[i] < dist) && (intree[i] == false)){
+                dist = distancia[i];
+                v = i;
+            }
+        }
+    }
+    cout << "Arvore geradora minima: " << endl;
+    for(i=0; i<nVertices; i++){
+        if(pai[i] != -1){
+            cout << pai[i] << " " << i << endl;
+            custo += distancia[i];
+        }
+    }
+    cout << "Custo: " << custo << endl;
+}
+
+int main(){
+
+    list<no> adj[10];     
+    int nVertices;        
+    int u, v, p;             //origem, destino e peso
+    list<no>::iterator q; 
+    int orientado;        
+    int inicial;          
+
+    cin >> nVertices >> orientado >> inicial;
+
+    cin >> u >> v >> p;
+    while (u != -1 && v != -1 && p != -1)
+    {
+        cria_aresta(adj, u, v, p, orientado);
+        cin >> u >> v >> p;
+    }
+
+    prim(adj, nVertices, inicial);
+
+    return 0;
+}
